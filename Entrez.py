2c6c0237dbd72cd9a215f3b698b349b2e0e6497c
Veloster122 <123640578+Veloster122@users.noreply.github.com>
2024-03-31 00:18:32 +0000

    Add files via upload
    First time adding this script to my repository

---
+++ Entrez.py
@@ -0,0 +1,37 @@
+import sys
+
+
+from Bio import Entrez
+from Bio import SeqIO
+
+Entrez.email = "202200065@estudantes.ips.pt"
+
+
+def sequence_gather(database, term):
+    handle = Entrez.esearch(db=database, term=term, usehistory="y")
+    record = Entrez.read(handle)
+    id_list = record["IdList"]
+    handle = Entrez.efetch(db=database, id=id_list, rettype="fasta", retmode="text")
+    sequences = list(SeqIO.parse(handle, "fasta"))
+    return sequences
+
+def main():
+    """
+    Main function to handle command-line arguments and retrieve sequences.
+    """
+    if len(sys.argv) != 4:
+        print("Usage: python script.py <database> <search_term> <output_file>")
+        sys.exit(1)
+
+    database = sys.argv[1]
+    search_term = sys.argv[2]
+    output_file = sys.argv[3]
+
+    sequences = sequence_gather(database, search_term)
+
+    with open(output_file, "w") as f:
+        SeqIO.write(sequences, f, "fasta")
+
+if __name__ == "__main__":
+    main()
+
